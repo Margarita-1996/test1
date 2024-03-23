a9c9005be81360c8c5dd68ca978a64e2dbe72cdd
Semenikhina Margarita <margarita.234@mail.ru>
2024-03-23 19:51:11 +0500

    first

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    public static void build() {
+        //Vyvodim tekst v konsol':
+        System.out.println("You must build a new car");
+    }
+    public static void buy() {
+        //Vyvodim tekst v konsol':
+        System.out.println("I will buy this car");
+    }
+    int carSpeed = 0;
+}
---
+++ src/newCar.java
@@ -0,0 +1,5 @@
+public class newCar {
+    public void build(){
+        System.out.println("You must build a new car");
+    }
+}
